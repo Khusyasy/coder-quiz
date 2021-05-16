9bf68990da0d1e2fd8ac1a6ba747b77bc4463e43
Muhammad Luthfi Khusyasy <khusyasy@gmail.com>
2021-05-16 14:15:06 +0700

    navbar dropdown create ButtonDropdown component use new dropdown to quiz and login menu remove LoginButton component

---
+++ client/src/components/ButtonDropdown.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="dropdown-container">
+    <span>
+      <slot name="btn"></slot>
+    </span>
+    <div class="dropdown" v-if="isOpen">
+      <slot name="dropdown"></slot>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "ButtonDropdown",
+  props: {
+    isOpen: Boolean,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../variables";
+
+.dropdown-container {
+  display: inline-block;
+  position: relative;
+  margin: 0;
+  padding: 0;
+  z-index: 1000;
+
+  & .dropdown {
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    row-gap: 0.5rem;
+    width: max-content;
+    right: 0;
+    top: 3rem;
+    padding: 1rem;
+    background-color: $green;
+    border: 1px solid $green-l;
+    border-radius: 5px;
+    transition: all 150ms ease-in-out;
+  }
+}
+</style>
